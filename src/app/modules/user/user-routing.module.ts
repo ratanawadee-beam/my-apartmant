b079d946fc125737e93a8024cf20eaeb3fe6e7ff
beam-beam <613170010108@rmu.ac.th>
2021-06-17 00:23:06 +0700

    เพิ่มserprofile

--- src/app/modules/user/user-routing.module.ts
+++ src/app/modules/user/user-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserPageComponent } from './user-page/user-page.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 const routes: Routes = [
   {
     path: '',
     component: UserPageComponent
+  },
+  {
+    path: 'profile',
+    component: UserProfileComponent
   }
+
 ];
 
 @NgModule({
--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { UserPageComponent } from './user-page/user-page.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 
 
 @NgModule({
   declarations: [
-    UserPageComponent
+    UserPageComponent,
+    UserProfileComponent
   ],
   imports: [
     CommonModule,
