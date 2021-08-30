11de8b914da595e0234c8a1c305968ee78c71c60
beam-beam <613170010108@rmu.ac.th>
2021-08-30 16:26:18 +0700

    [update]regit

--- src/app/modules/admin/admin-reports/admin-reports.component.ts
+++ src/app/modules/admin/admin-reports/admin-reports.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AdminService } from 'src/app/shared/service/admin.service';
 
 @Component({
   selector: 'app-admin-reports',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-reports.component.css']
 })
 export class AdminReportsComponent implements OnInit {
-
-  constructor() { }
+  
+  listInvoice: any;
+  constructor(
+    private adminService: AdminService,
+  ) { }
 
   ngOnInit(): void {
+    this.invoiceData();
+  }
+
+  invoiceData() {
+    this.adminService.getAllInvoice().subscribe(   
+      (res) => {
+        console.log('!!!!!! Rent Data !!!!!!',res)
+        this.listInvoice = res;
+      },
+      (error) => {
+        console.log('!!!!!! Rent Data !!!!!!',error);
+      }
+    );
   }
 
 }
